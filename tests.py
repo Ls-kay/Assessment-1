94a112cff8e4f8c15070de4cd3c2b8ead49ebb0c
Lebohang M <msaselebohang@gmail.com>
2024-11-18 11:08:58 +0200

    Wrote a few programs with unit tests

--- tests.py
+++ tests.py
@@ -21,8 +21,8 @@ class TestCase(unittest.TestCase):
     def test_interest(self):
         # write tests that cater for the output being an integer, unexpected outputs and expected ouputs
         self.assertIsInstance(calculate_interest(2000, 15/100, 5), int, "Expected an integer")
-        self.assertEqual(calculate_interest(2), (2), None)
-        se
+        self.assertEqual(calculate_interest(2), 2, None)
+        self.assertEqual(calculate_interest(300, 14/1000, "Unexpected output"))
         pass
 
 
